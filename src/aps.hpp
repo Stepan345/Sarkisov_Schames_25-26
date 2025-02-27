a1f74c13d530bd18a35aba6fd2610fc96df42578
Stepan345 <79114505+Stepan345@users.noreply.github.com>
2025-02-26 20:36:51 -0500

    first commit

---
+++ src/aps.hpp
@@ -0,0 +1,46 @@
+#include <cmath>
+struct position{
+    double x;
+    double y;
+    double heading;
+};
+position globalPos = {0,0,0};
+
+position getGlobalPosition(){
+    return globalPos;
+}
+double sL;
+double sR;
+double sB;
+
+void setOffsets(double offsetL, double offsetR, double offsetB){
+    sL = offsetL;
+    sR = offsetR;
+    sB = offsetB;
+}
+
+double getLocalYTransform(double deltaL,double deltaR){
+    if(deltaL == deltaR){
+        //if the robot went in a straight line, just return the distance.
+        return deltaL;
+    }
+    //change in heading
+    double deltaTheta = (deltaL-deltaR)/(sL+sR);
+    
+    double arcLength = (deltaR/deltaTheta)+sR;
+
+    double chordLength = 2* std::sin(deltaTheta/2)*arcLength;
+    return chordLength;
+}
+double getLocalXTransform(double deltaL,double deltaR,double deltaB){
+    double deltaTheta = (deltaL-deltaR)/(sL+sR);
+    //expected travel if the turn had no drift
+    double perfectDistance = 2 * M_PI * sB * (deltaTheta/2*M_PI);
+
+    double drift = deltaB-perfectDistance;
+    return drift;
+}
+double updateGlobalTransform(double deltaL,double deltaR,double deltaB){
+
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "main.h"
+using namespace pros;
+
+
+void competition_initialize(){
+	//LCD Autonomous Selector
+}
+Controller Controller1(E_CONTROLLER_MASTER);
+MotorGroup left_mg({1, -2, 3});
+MotorGroup right_mg({-4, 5, -6});
+void autonomous(){
+
+}
+
+void opcontrol(){
+	
+
+
+	while (true) {
+		
+	}
+}
