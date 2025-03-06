66239cc3eb19cb298850a57571b8902429fe2ed7
Stepan345 <79114505+Stepan345@users.noreply.github.com>
2025-03-06 16:06:09 -0500

    added path following

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ void competition_initialize(){
 	//LCD Autonomous Selector
 }
 Controller Controller1(E_CONTROLLER_MASTER);
-// MotorGroup LeftDrive({-18, -19, -20}, pros::MotorGearset::blue);
-// MotorGroup RightDrive({8, 9, 10}, pros::MotorGearset::blue);
-
+ASSET(path_txt);
 void autonomous(){
-
+    chassis.setPose(0, 0, 0);
+    chassis.follow(path_txt,10,60000);
+    //chassis.turnToHeading(90,1000000);
 }
 void initialize(){
 	init();
